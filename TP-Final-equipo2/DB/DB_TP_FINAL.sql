3fda1c89f8b87be910ff529c254d919678ffeac8
CristianLeopardo <cristianleopardo@gmail.com>
2023-06-14 15:00:32 -0300

    V1.3

--- TP-Final-equipo2/DB/DB_TP_FINAL.sql
+++ TP-Final-equipo2/DB/DB_TP_FINAL.sql
@@ -7,3 +7,29 @@ create table Usuarios(
     Clave varchar(50) not null,
     TipoUsuario int not null
 )
+go
+create table Medicos(
+    ID int not null PRIMARY KEY IDENTITY (1,1),
+    Nombre VARCHAR(100) not null,
+    Apellido VARCHAR(100) not null,
+    IDEspecialidad int not null
+)
+GO
+create table Pacientes(
+    ID int not null PRIMARY KEY IDENTITY (1,1),
+    Nombre VARCHAR(100) not null,
+    Apellido VARCHAR(100) not null
+)
+GO
+create table Turnos(
+    ID int not null PRIMARY KEY IDENTITY (1,1),
+    IDPaciente int not null,
+    IDMedico int  not null,
+    IDEspecialidad int not null,
+    Fecha datetime not null,
+    Estado int not null
+)
+create table Especialidad(
+    ID int not null PRIMARY KEY IDENTITY (1,1),
+    Nombre VARCHAR(100) not null
+)
